030a0fde9f18bde8ae70d17c52be25f77176d9df
Chris Hallberg <chris.hallberg@hey.com>
2025-12-03 18:58:11 -0500

    doc: explain method.

--- Project-Euler/Problem005.js
+++ Project-Euler/Problem005.js
@@ -4,9 +4,11 @@ import { PrimeFactors } from '../Maths/PrimeFactors.js'
  * Smallest Multiple
  * @link https://projecteuler.net/problem=5
  *
- * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  *
- * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * Method: unique factorization
+ * @link https://en.wikipedia.org/wiki/Least_common_multiple#Using_prime_factorization
+ * The method used below calculates the Least Common Multiple (LCM) by multiplying the largest powers of the prime factors of the divisors. For example, 8 is the LCM of 2, 4, and 8. The prime factors of these numbers are 2^1, 2^2, and 2^3. We can discard 2^1 and 2^2 since 2^3 is the largest power of 2, leaving us with a result of 2^3 (8).
  */
 
 export function findSmallestMultiple(maxDivisor) {
